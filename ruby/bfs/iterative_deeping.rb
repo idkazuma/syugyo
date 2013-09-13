c5e7e3762031db62b9f22c93f7ab3ab62c64306f
idkazuma <idkazuma@gmail.com>
2013-09-13 11:08:10 +0000

    add iterative_deeping.rb

---
+++ ruby/bfs/iterative_deeping.rb
@@ -0,0 +1,92 @@
+
+#   - b - d - f
+# a   |   |
+#   - c - e - g
+#
+#   - 1 - 3 - 5
+# 0   |   |
+#   - 2 - 4 - 6
+#
+
+START=0
+
+GOAL=6
+Loadmap=[]
+"12,023,014,145,236,3,4".split(",").each do |x|
+#   loadmap.push(x.split(//).to_s)
+  Loadmap.push(x.split(//))
+end
+
+# p loadmap[0]
+# puts load
+
+def search_test(goal, paths)
+  now = paths[paths.size-1].to_i
+  if now == goal
+    p paths
+  else
+    # p paths
+    # p Loadmap[now]
+    # p Loadmap[now]
+    Loadmap[now].each do |n|
+     if !paths.include? n
+       # puts n
+       paths.push n
+       search_test GOAL, paths
+       paths.pop
+     end
+   end
+  end
+#   # paths[paths.len
+#   # puts paths.len
+end
+
+
+# iterative deeping 反復深化
+def iterative_deeping(limit, goal, paths)
+  if paths.size <= limit
+    p"SIZE=" + paths.size.to_s
+    now = paths[paths.size-1].to_i
+    if now == goal
+      p paths
+    else
+      Loadmap[now].each do |n|
+        if !paths.include? n
+          # puts n
+          paths.push n
+          iterative_deeping limit, GOAL, paths
+          paths.pop
+        end
+      end
+    end
+  end
+end
+
+def bfs(start, goal)
+  # enqueue
+  q=[[start.to_s]]
+  while q.size > 0
+    # dequeue
+    paths = q.shift
+    now = paths[paths.size-1].to_i
+    if now == goal
+      p paths
+      # uncomment when need just only one
+      # return
+    else
+      Loadmap[now].each do |n|
+        if !paths.include? n
+          newpaths = Marshal.load(Marshal.dump(paths))
+          newpaths.push n
+          # enqueue
+          q.push newpaths
+        end
+      end
+    end
+  end
+end
+
+# search_test(GOAL, ["0"])
+# bfs(START, GOAL)
+iterative_deeping(5, GOAL, ["0"])
+
